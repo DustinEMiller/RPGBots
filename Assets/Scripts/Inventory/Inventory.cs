bb5adc7c876fba732d6469e94478e50b6a214c36
Dustin Miller <dustin.miller@carle.com>
2021-07-27 17:49:58 -0500

    Added item slot
    Added item slot

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public List<Item> Items = new List<Item>();
+    private const int GENERAL_SIZE = 9;
+    public ItemSlot[] GeneralInventory = new ItemSlot[GENERAL_SIZE];
     [SerializeField] private Item _debugItem;
 
+    void Awake()
+    {
+        for (int i = 0; i < GENERAL_SIZE; i++)
+            GeneralInventory[i] = new ItemSlot();
+    }
     public void AddItem(Item item)
     {
-        Items.Add(item);
+        var firstAvailableSlot = GeneralInventory.FirstOrDefault(t=> t.IsEmpty);
+        firstAvailableSlot.SetItem(item);
     }
 
     [ContextMenu(nameof(AddDebugItem))]
